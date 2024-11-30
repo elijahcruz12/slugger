09a56c870c87e70beee654fae5ab08358312b8d8
Elijah Cruz <eli@theecgaming.com>
2024-11-30 11:39:29 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Slugger
+
+** THIS PROJECT IS A WORK IN PROGRESS, DO NOT USE IT **
+
+Slugger is a simple PHP program that renames files and directories to a slug format.
+
+The goal is to be able to rename files and directories to a slug format. This is useful for SEO purposes, and for creating clean URLs. I also want to make it possible to transform images using Imagick, but with a CLI, that feels easier to use.
---
+++ app/Commands/RunSlugCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class RunSlugCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'slug:run {--all : Run on all FILES in the current directory} {--file= : Run on a specific FILE in the current directory} {--dry : Dry run}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run the slug command on a file or all files in the current directory';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if($this->option('all')) {
+            $files = array_diff(scandir(getcwd()), ['.', '..']);
+            foreach($files as $file) {
+                $this->runSlug($file);
+            }
+        } else if($this->option('file')) {
+            $this->runSlug($this->option('file'));
+        } else {
+            $this->error('No file specified');
+        }
+    }
+
+    private function runSlug(?string $file = null)
+    {
+        $this->info($file ?? 'No file specified');
+    }
+}
