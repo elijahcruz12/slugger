991c986209298a845feb73a7cd7770da03693db4
Elijah Cruz <eli@theecgaming.com>
2024-11-30 11:53:43 -0800

    Actually made it work

--- app/Commands/RunSlugCommand.php
+++ app/Commands/RunSlugCommand.php
@@ -3,6 +3,7 @@
 namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Str;
 use LaravelZero\Framework\Commands\Command;
 
 class RunSlugCommand extends Command
@@ -27,11 +28,18 @@ class RunSlugCommand extends Command
     public function handle()
     {
         if($this->option('all')) {
-            $files = array_diff(scandir(getcwd()), ['.', '..']);
+            // Get only files in the current directory
+            $files = array_filter(scandir(getcwd()), function($file) {
+                return is_file($file);
+            });
             foreach($files as $file) {
                 $this->runSlug($file);
             }
         } else if($this->option('file')) {
+            if(!is_file($this->option('file'))) {
+                $this->error('File does not exist');
+                return;
+            }
             $this->runSlug($this->option('file'));
         } else {
             $this->error('No file specified');
@@ -42,8 +50,39 @@ class RunSlugCommand extends Command
     {
         // Make sure file is not a directory
         if(is_dir($file)) {
-            $this->error('File is a directory');
             return;
+        }
+
+        // Get just the filename (no path, no extension)
+        $filename = pathinfo($file, PATHINFO_FILENAME);
+
+        // Get the file extension
+        $extension = pathinfo($file, PATHINFO_EXTENSION);
+
+        // There may be multiple dots in the filename, so we need to slugify the filename
+        $slug = Str::slug($filename,
+            dictionary: [
+                '@' => 'at',
+                '.' => '.',
+                '_' => '-',
+                ' ' => '-',
+            ]
+        );
+
+        // If the file has an extension, append it to the slug
+        if($extension) {
+            $slug .= '.' . $extension;
+        }
+
+        $this->info($slug);
+
+        if(!$this->option('dry')) {
+            // Rename the file
+            rename($file, $slug);
+            $this->info('Renamed ' . $file . ' to ' . $slug);
+        }
+        else {
+            $this->info('Dry run, not renaming ' . $file . ' to ' . $slug);
         }
     }
 }
